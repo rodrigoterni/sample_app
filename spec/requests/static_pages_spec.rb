3c80f842407167f125fb09072a0ef92c5709696f
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-25 16:01:59 -0300

    Add contact page - Question: Why not tested controller?

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,5 +35,16 @@ describe "Static pages" do
       page.should have_selector('title', :text => " | About Us")
     end
   end
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => " | Contact")
+    end
+  end
 
 end
