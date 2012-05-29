4ec75be712f5454120095d10dcdec6aa2f0ab685
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-29 12:33:44 -0300

    Why is there a need for user_path(user) in delete?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,16 @@ describe User do
 
   it { should respond_to(:authenticate) }
 
+  it { should respond_to(:admin) }
+
+  it { should_not be_admin }
   it { should be_valid }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
 
   # Name Validations
 
