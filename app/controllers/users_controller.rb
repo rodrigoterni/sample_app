c163a438508584204420d27d11050e3119e519e8
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-28 23:48:53 -0300

    Question: iteration through User.all in tests only uses test db?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -17,4 +24,29 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
