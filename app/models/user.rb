2a9368b13e471f2f88ef756a3452cc058910fa9f
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-30 00:07:15 -0300

    Add user microposts Problem: Layout Question: Why not on controller Feed method?

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   before_save { |user| user.email = email.downcase}
   before_save :create_remember_token
@@ -23,6 +25,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
   	def create_remember_token
